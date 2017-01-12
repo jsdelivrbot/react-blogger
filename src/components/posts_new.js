40aaaa7f565f8b0e687a1e2c938d6734fbb8eff8
root <gerard.ramosm89@gmail.com>
2017-01-12 11:42:50 -0800

    Form Submission create

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,11 +1,36 @@
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 
 class PostsNew extends Component {
 	render(){
+		const { fields: { title, categories, content }, handleSubmit } = this.props;
+		console.log(title);
+		// If you didn't want to do deconstruction you could also just do
+		// const title = this.props.fields.title; etc.
 		return(
-			<div> Create Form </div>
+			<div>
+			<form onSubmit="handleSubmit">
+	<h3>Create a new post</h3>
+	<div className="form-group">
+		<label>Title</label>
+		<input type="text" className="form-control" />
+	</div>
+  <div className="form-group">
+    <label>Categories</label>
+    <input type="text" className="form-control" />
+  </div>
+  <div className="form-group">
+    <label>Content</label>
+    <textarea type="text" className="form-control" />
+  </div>
+				<button type="submit" className="btn btn-primary">Submit</button>
+			</form>
+			</div>
 		);
 	}
 }
 
-export default PostsNew;
+export default reduxForm({
+	form: 'PostsNewForm',
+	fields: ['title', 'categories', 'content']
+})(PostsNew);
