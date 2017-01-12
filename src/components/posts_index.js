d16b26dd87101c4f3b32e3de2fe82ed9c050a1e7
root <gerard.ramosm89@gmail.com>
2017-01-12 09:30:14 -0800

    working on adding posts

--- src/components/posts_index.js
+++ src/components/posts_index.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 import { fetchPosts } from '../actions/index';
+import { Link } from 'react-router';
 
 class PostsIndex extends Component {
 	componentWillMount(){
@@ -9,12 +9,17 @@ class PostsIndex extends Component {
 	}
 	render(){
 		return (
-			<div>List of blog posts</div>
+			<div>
+				<div className="text-xs-right">
+					<Link to="posts/new" className="btn btn-info">
+					Add a post
+					</Link>
+				</div>
+			List of blog posts
+			
+			</div>
 		);
 	}
 }
 
-function mapDispatchToProps(dispatch) {
-	return bindActionCreators({ fetchPosts } , dispatch);
-}
-export default connect(null, mapDispatchToProps)(PostsIndex);
+export default connect(null, { fetchPosts })(PostsIndex);
